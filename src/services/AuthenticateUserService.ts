73a30a0974e81ed090094583490d174b2ae04c54
André R. de Melo <andrermelo1@hotmail.com>
2022-04-12 01:30:52 -0300

    aula 1 parte 2 pronta

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
+import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken";
 /**
  * Receber o code(string) - V
  * Recuperar o access_token no github - V
  * Recuperar infos do user no github - V
- * Verificar se o usuário existe no DB
+ * Verificar se o usuário existe no DB - V
  * ----- SIM = Gera um token
  * ---- NÃO = Cria no DB, gera um token
  * Retornar o token com as infos do user
@@ -45,7 +47,43 @@ class AuthenticateUserService {
       }
     );
 
-    return response.data;
+    const { login, avatar_url, id, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id, // name_colum: equal value
+      },
+    });
+
+    if (!user) {
+      await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    const token = sign(
+      {
+        //primeiro parametro é um payload,
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET, // Uma secret, que serve para criar o token e validar ele
+      {
+        // subject normalmente é o id do usuario
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { token, user };
   }
 }
 
